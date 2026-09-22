a7b103eab8a20506a8dd34f5549a00dfbccf4e7a
nusivaishnavkumarreddy <nusivaishnavkumarreddy@gmail.com>
2026-09-22 17:14:51 +0530

    Delete lib directory

--- lib/components/Splash.tsx
+++
@@ -1,9 +0,0 @@
-export default function Splash({ text = "Warming up your cozy corner..." }: { text?: string }) {
-  return (
-    <div className="splash-wrap">
-      <img src="/kizora-icon.png" alt="Kizora" />
-      <h2>Kizora</h2>
-      <p>{text}</p>
-    </div>
-  );
-}
